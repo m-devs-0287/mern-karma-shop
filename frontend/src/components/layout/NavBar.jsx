9842e33533ff84df5b039905e12c0c6532543501
manuels-code <113308860+m-devs-0287@users.noreply.github.com>
2023-01-24 18:05:07 -0600

    Update NavBar.jsx

--- frontend/src/components/layout/NavBar.jsx
+++ frontend/src/components/layout/NavBar.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { IoSearchOutline } from "react-icons/io5";
+import { AiOutlineShopping } from "react-icons/ai";
 
 const menuLinks = [
   {
@@ -7,27 +9,27 @@ const menuLinks = [
     path: "/",
   },
   {
-    id: 1,
+    id: 2,
     title: "shop",
     path: "/shop  ",
   },
   {
-    id: 1,
+    id: 3,
     title: "blog",
     path: "/blog",
   },
   {
-    id: 1,
+    id: 4,
     title: "pages",
     path: "/pages",
   },
   {
-    id: 1,
+    id: 5,
     title: "contact",
     path: "/contact",
   },
   {
-    id: 1,
+    id: 6,
     title: "store",
     path: "/store",
   },
@@ -36,22 +38,24 @@ const menuLinks = [
 const NavBar = () => {
   return (
     <nav className="flex h-full items-center text-sm">
-
       <ul className="flex items-center uppercase">
         {menuLinks.map(({ id, title, path }) => (
-          <li key={id} className='px-6'>
+          <li key={id} className="px-6">
             <a href={path}>{title}</a>
           </li>
         ))}
         <li>
           <span className="absolute -left-[1000px]">search</span>
-          <a href="/"><span class="material-symbols-outlined text-base">local_mall</span></a></li>
+          <a href="/">
+            <AiOutlineShopping className="text-xl" />
+          </a>
+        </li>
       </ul>
 
       <div>
         <button className="flex h-full items-center px-6 ">
           <span className="absolute -left-[1000px]">store</span>
-          <span class="material-symbols-outlined text-md">search</span>
+          <IoSearchOutline className="text-xl" />
         </button>
       </div>
     </nav>

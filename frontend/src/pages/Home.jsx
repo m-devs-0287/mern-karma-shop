6048423de160e922e7a1199bfc112db3b1936c80
manuels-code <113308860+m-devs-0287@users.noreply.github.com>
2023-01-26 01:01:17 -0600

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import FeaturesArea from "../components/features-area/FeaturesArea";
+import GalleryOfDeals from "../components/gallery-deals-area/GalleryOfDeals";
 import Hero from "../components/Hero";
 
 const Home = () => {
@@ -7,6 +8,7 @@ const Home = () => {
     <>
       <Hero />
       <FeaturesArea />
+      <GalleryOfDeals />
     </>
   );
 };

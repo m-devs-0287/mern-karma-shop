835897a0d53d7924eab50cb884740ca866a47b9d
manuels-code <113308860+m-devs-0287@users.noreply.github.com>
2023-01-26 01:04:49 -0600

    Update GalleryOfDeals.jsx

--- frontend/src/components/gallery-deals-area/GalleryOfDeals.jsx
+++ frontend/src/components/gallery-deals-area/GalleryOfDeals.jsx
@@ -29,7 +29,7 @@ const dealsItems = [
 
 const GalleryOfDeals = () => {
   return (
-    <div className="flex h-[420px] w-[1200px] m-auto px-4 mb-24">
+    <div className="flex h-[420px] w-[1200px] px-6 m-auto mb-24">
       <div className="h-full grid grid-cols-3 gap-8 mr-8 mb-24 justify-between w-[65%] ">
         {dealsItems.map(({ id, img, path, context }) => (
           <a
@@ -50,8 +50,8 @@ const GalleryOfDeals = () => {
         ))}
       </div>
 
-      <div className="w-[35%] ">
-        <a href="/" className="drop-shadow-lg overflow-hidden inline-block group">
+      <div className="w-[35%]">
+        <a href="/" className="drop-shadow-lg overflow-hidden inline-block group w-full h-full">
           <img src="./assets/c5.jpg.webp" alt="" className="w-full h-full" />
 
           <div className="group-hover:bg-black/50 absolute h-full w-full left-0 top-0 flex justify-center items-center duration-500 ease-in-out">

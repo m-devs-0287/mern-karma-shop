714aaecfc21e84bf67deced884f048cd8af613c5
manuels-code <113308860+m-devs-0287@users.noreply.github.com>
2023-01-28 23:34:25 -0600

    Update App.js
    - Adding react-router-dom path for Store.jsx, Cart.jsx, Blog.jsx, Contact.jsx

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,24 @@
+import { Routes, Route } from "react-router-dom";
 import Footer from "./components/layout/Footer";
 import Header from "./components/layout/Header";
 import Home from "./pages/Home";
+import Store from "./pages/Store";
+import Cart from "./pages/Cart";
+import Blog from "./pages/Blog";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
     <div className="text-gray-500">
       <Header />
       <main>
-        <Home />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
       </main>
       <Footer />
     </div>

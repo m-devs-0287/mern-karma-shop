044c77e806183afc918c8b0a46c4db3f0d5c3e62
manuels-code <113308860+m-devs-0287@users.noreply.github.com>
2023-01-28 23:32:43 -0600

    Update Hero.jsx
    - Making Hero.jsx reusable for other pages

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,12 +1,32 @@
+import { useEffect, useState } from "react";
 import React from "react";
-import Slider from './image-slider/ImageSlider'
+import Slider from "./image-slider/ImageSlider";
+import Breadcrumbs from "./breadcrumbs/Breadcrumbs";
+
 const Hero = () => {
+  const [url, setUrl] = useState("");
+
+  useEffect(() => {
+    const currentUrl = window.location.href;
+    setUrl(currentUrl);
+  }, []);
+
   return (
-    <section className="h-screen bg-hero bg-cover bg-center">
-      <div className="m-auto w-[1200px] h-full">
-        <Slider />
-      </div>
-    </section>
+    <>
+      {url === "http://localhost:3000/" ? (
+        <section className="h-screen bg-hero bg-cover bg-center">
+          <div className="m-auto w-[1200px] h-full">
+            <Slider />
+          </div>
+        </section>
+      ) : (
+        <section className="h-[400px] bg-hero bg-cover bg-center">
+          <div className="m-auto w-[1200px] h-full">
+            <Breadcrumbs />
+          </div>
+        </section>
+      )}
+    </>
   );
 };
 

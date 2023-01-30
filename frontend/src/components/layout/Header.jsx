c70c895c84bf5bf4e21038f6189699883f37b3e5
manuels-code <113308860+m-devs-0287@users.noreply.github.com>
2023-01-29 20:25:12 -0600

    Adding animation Header.jsx on scroll

--- frontend/src/components/layout/Header.jsx
+++ frontend/src/components/layout/Header.jsx
@@ -1,14 +1,34 @@
 import React from "react";
+import { useState } from "react";
 import NavBar from "./NavBar";
 
 const Header = () => {
+  const [fixedHeader, setFixedHeader] = useState(false);
+
+  window.document.addEventListener("scroll", () => {
+    if (window.pageYOffset > 64) {
+      setFixedHeader(true);
+    } else {
+      setFixedHeader(false);
+    }
+  });
+
+  const styles = {
+    header: fixedHeader ? `fixed top-0` : `absolute top-16 `,
+    headerContainer: fixedHeader ? `w-full` : `w-[1200px]`,
+  };
+
   return (
-    <header className="fixed w-full flex justify-center top-11 z-50">
-      <div className="bg-white h-20 px-10 flex justify-between items-center w-[1200px] drop-shadow-lg">
-        <div className="logo">
-          <img src="./assets/logo.png.webp" alt="" />
+    <header className={`z-[50] w-full ${styles.header}`}>
+      <div
+        className={`duration-150 ease-linear bg-white drop-shadow-lg -left-full -right-full m-auto  ${styles.headerContainer}`}
+      >
+        <div className="flex justify-between h-20 px-10 items-center w-full max-w-[1200px] -left-full -right-full m-auto">
+          <div className="logo">
+            <img src="./assets/logo.png.webp" alt="" />
+          </div>
+          <NavBar />
         </div>
-        <NavBar />
       </div>
     </header>
   );

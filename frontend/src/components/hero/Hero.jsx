9bb3d19a2ab75fd81b5b3f7c371c60cf2edf2617
manuels-code <113308860+m-devs-0287@users.noreply.github.com>
2023-01-30 23:55:29 -0600

    Update Hero.jsx

--- frontend/src/components/hero/Hero.jsx
+++ frontend/src/components/hero/Hero.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import React from "react";
-import Slider from "../image-slider/ImageSlider";
+import Slider from "../image-slider/Slider";
 import Breadcrumbs from "../breadcrumbs/Breadcrumbs";
 
 const Hero = (props) => {
@@ -14,15 +14,17 @@ const Hero = (props) => {
   return (
     <>
       {url === "http://localhost:3000/" ? (
-        <section className="h-screen bg-hero bg-cover bg-center">
+      
+      <section className="h-screen bg-hero bg-cover bg-center">
           <div className="m-auto w-[1200px] h-full">
             <Slider />
           </div>
         </section>
+
       ) : (
-        <section className="h-[400px] bg-hero bg-[100%] bg-[bottom_100px]">
-          <div className="m-auto w-[1200px] h-full flex flex-col items-end top-1/2 relative">
-            <div className="w-1/2 text-white flex flex-col gap-4 capitalize">
+        <section className="h-[400px] bg-hero bg-[100%] bg-[bottom_100px] overflow-hidden">
+          <div className="m-auto w-[1200px] flex flex-col items-end top-1/2 relative">
+            <div className="w-1/2 text-white flex flex-col gap-4 capitalize border">
               <h1 className="text-5xl font-bold capitalize">
                 {props.page} Page
               </h1>

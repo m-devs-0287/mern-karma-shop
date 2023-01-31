a7e97f7090862f6c7bbd1bea7070417b8b31394c
manuels-code <113308860+m-devs-0287@users.noreply.github.com>
2023-01-30 23:57:21 -0600

    Update Store.jsx

--- frontend/src/pages/Store.jsx
+++ frontend/src/pages/Store.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import Categories from "../components/categories/Categories";
 import Hero from "../components/hero/Hero";
-
 
 const Store = () => {
   return (
     <>
-      <Hero page={'store'}/>
+      <Hero page={"store"} />
+      <Categories />
     </>
   );
 };

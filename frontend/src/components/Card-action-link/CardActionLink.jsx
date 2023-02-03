6619b7c7f1c0cf164b897ce1749ee704aa52c2e9
manuels-code <113308860+m-devs-0287@users.noreply.github.com>
2023-02-02 23:34:45 -0600

    refacturing component
    - moved logic inside the component
    - change approach for loading react icons

--- frontend/src/components/Card-action-link/CardActionLink.jsx
+++ frontend/src/components/Card-action-link/CardActionLink.jsx
@@ -1,32 +1,54 @@
 import React from "react";
-import { AiOutlineShopping, AiOutlineHeart } from "react-icons/ai";
-import { BsArrowsMove } from "react-icons/bs";
-import { TiArrowSync } from "react-icons/ti";
+import * as Ai from "react-icons/ai";
+import * as Ti from "react-icons/ti";
+import * as Bs from "react-icons/bs";
 
-const style = {
+const data = [
+  {
+    id: 1,
+    path: "/",
+    icon: <Ai.AiOutlineShopping />,
+    context: "Add to bag",
+  },
+  {
+    id: 2,
+    path: "/",
+    icon: <Ai.AiOutlineHeart />,
+    context: "Wish List",
+  },
+  {
+    id: 3,
+    path: "/",
+    icon: <Ti.TiArrowSync />,
+    context: "Compare",
+  },
+  {
+    id: 4,
+    path: "/",
+    icon: <Bs.BsArrowsMove />,
+    context: "View More",
+  },
+];
+
+const styles = {
+  a: "flex items-center whitespace-nowrap relative overflow-hidden px-1",
   icon: `bg-[#828cb1] h-8 w-8 p-2 rounded-full text-white relative z-[2] mr-[0px] group-hover:mr-[90px] group-hover:bg-gradient-to-r from-yellow-300 to-orange-500 ease-in-out duration-300`,
+  context:
+    "absolute -left-[10px] z-[1] invisible opacity-0 group-hover:left-[40px] group-hover:visible group-hover:opacity-100 ease-in-out duration-300 font-semibold",
 };
 
-const CardActionLink = ({ path, icon, context }) => {
+const CardActionLink = () => {
   return (
-    <a
-      className="flex items-center whitespace-nowrap relative overflow-hidden px-1"
-      href={path}
-    >
-      {icon === "AiOutlineShopping" ? (
-        <AiOutlineShopping className={style.icon} />
-      ) : icon === "AiOutlineHeart" ? (
-        <AiOutlineHeart className={style.icon} />
-      ) : icon === "BsArrowsMove" ? (
-        <BsArrowsMove className={style.icon} />
-      ) : icon === "TiArrowSync" ? (
-        <TiArrowSync className={style.icon} />
-      ) : null}
-
-      <span className="absolute -left-[10px] z-[1] invisible opacity-0 group-hover:left-[40px] group-hover:visible group-hover:opacity-100 ease-in-out duration-300 font-semibold">
-        {context}
-      </span>
-    </a>
+    <ul className="flex">
+      {data.map(({ id, path, icon, context }) => (
+        <li key={id} className="group">
+          <a className={styles.a} href={path}>
+            <span className={styles.icon}>{icon}</span>
+            <span className={styles.context}>{context}</span>
+          </a>
+        </li>
+      ))}
+    </ul>
   );
 };
 

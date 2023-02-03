a5b8f15eb0a531f67e1291644c640876e41828c2
manuels-code <113308860+m-devs-0287@users.noreply.github.com>
2023-02-02 23:36:50 -0600

    Refactoring code for Categories.jsx
    - remove unnecessary object of data

--- frontend/src/components/categories/Categories.jsx
+++ frontend/src/components/categories/Categories.jsx
@@ -2,127 +2,7 @@ import React from "react";
 import AsideMenu from "../browser-categories/AsideMenu";
 import ProductCard from "../product-card/ProductCard";
 import SearchFilterbar from "../search-filter-bar/SearchFilterbar";
-
-// ProductCard component data
-const latestProducts = [
-  {
-    id: 1,
-    img: "./assets/p1.jpg.webp",
-    brand: "Nike",
-    model: "Hammer Sole",
-    price: 210,
-    salesPrice: 150,
-    type: "Sports",
-    color: 'grey',
-    gender: 'men'
-  },
-  {
-    id: 2,
-    img: "./assets/p2.jpg.webp",
-    brand: "Nike",
-    model: "Hammer Sole",
-    price: 210,
-    salesPrice: 150,
-    type: "Running",
-    color: 'green',
-    gender: 'women'
-  },
-  {
-    id: 3,
-    img: "./assets/p3.jpg.webp",
-    brand: "Nike",
-    model: "Hammer Sole",
-    price: 210,
-    salesPrice: 150,
-    type: "casual",
-    color: 'black',
-    gender: 'women'
-  },
-  {
-    id: 4,
-    img: "./assets/p4.jpg.webp",
-    brand: "Nike",
-    model: "Hammer Sole",
-    price: 210,
-    salesPrice: 150,
-    type: "classic",
-    color: 'brown',
-    gender: 'men'
-  },
-  {
-    id: 5,
-    img: "./assets/p5.jpg.webp",
-    brand: "Nike",
-    model: "Hammer Sole",
-    price: 210,
-    salesPrice: 150,
-    type: "casual",
-    color: 'grey',
-    gender: 'men'
-  },
-  {
-    id: 6,
-    img: "./assets/p6.jpg.webp",
-    brand: "Nike",
-    model: "Hammer Sole",
-    price: 210,
-    salesPrice: 150,
-    type: "Sports",
-    color: 'red',
-    gender: 'kids'
-  },
-  {
-    id: 7,
-    img: "./assets/p7.jpg.webp",
-    brand: "Nike",
-    model: "Hammer Sole",
-    price: 210,
-    salesPrice: 150,
-    type: "classic",
-    color: 'grey',
-    gender: 'women'
-  },
-  {
-    id: 8,
-    img: "./assets/p8.jpg.webp",
-    brand: "Nike",
-    model: "Hammer Sole",
-    price: 210,
-    salesPrice: 150,
-    type: "casual",
-    color: 'grey',
-    gender: 'women'
-  },
-  
-];
-
-const actionLinks = [
-  {
-    id: 1,
-    path: "/",
-    icon: "AiOutlineShopping",
-    context: "Add to bag",
-  },
-  {
-    id: 2,
-    path: "/",
-    icon: "AiOutlineHeart",
-    context: "Wish List",
-  },
-  {
-    id: 3,
-    path: "/",
-    icon: "TiArrowSync",
-    context: "Compare",
-  },
-  {
-    id: 4,
-    path: "/",
-    icon: "BsArrowsMove",
-    context: "View More",
-  },
-  
-];
+import Products from '../../data/products.json'
 
 //Categories component styles
 const styles = {
@@ -138,7 +18,7 @@ const Categories = () => {
       <div className={styles.productsContainer}>
         <SearchFilterbar />
         <div className={styles.productWrapper}>
-          <ProductCard latestProducts={latestProducts} actionLinks={actionLinks}/>
+          <ProductCard data={Products}/>
         </div>
       </div>
     </div>

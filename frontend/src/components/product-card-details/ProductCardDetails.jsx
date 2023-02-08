91604d27a28c1054e41e537ae4efef61ca44f35c
Manuel Nunez <manuelnunezalmonte@gmail.com>
2023-02-07 23:45:42 -0600

    Create ProductCardDetails.jsx

---
+++ frontend/src/components/product-card-details/ProductCardDetails.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import CardActionLink from "../Card-action-link/CardActionLink";
+
+
+const styles = {
+  wrapper: "flex flex-col gap-3",
+  imgContainer: "w-full h-[255px]",
+  img: "w-full h-full",
+  h3: "font-semibold",
+  p: "flex gap-4 font-semibold",
+  pSpan: "line-through text-black/20",
+};
+
+const ProductCardDetails = ({data}) => {
+  const { id, img, brand, model, type, price, salesPrice } = data;
+  
+  return (
+    
+    <div className={styles.wrapper}>
+      
+      <div className={styles.imgContainer}>
+        <img src={img} alt="" className={styles.img} />
+      </div>
+
+      <div>
+        <h3 className={styles.h3}>
+          {brand} {model} {type}
+        </h3>
+        <p className={styles.p}>
+          <span>${salesPrice}</span>
+          <span className={styles.pSpan}>{price}</span>
+        </p>
+      </div>
+      <CardActionLink />
+    </div>
+    
+  );
+};
+
+export default ProductCardDetails;

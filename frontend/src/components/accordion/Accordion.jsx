3f55be3fe5d1d7f7c7d1e5061d302b87aaee61d5
Manuel Nunez <manuelnunezalmonte@gmail.com>
2023-02-08 23:39:18 -0600

    Update Accordion.jsx
    include UseUpdateFilter
    add updateFilter function to submenu items

--- frontend/src/components/accordion/Accordion.jsx
+++ frontend/src/components/accordion/Accordion.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { useState } from "react";
+import { UseUpdateFilter } from "../../hooks/FilterContext";
 
-const Accordion = ({ header,data }) => {
+const Accordion = ({ header, data }) => {
+  const updateFilter = UseUpdateFilter();
+
   const [selected, setSelected] = useState(null);
 
   const toggle = (index) => {
@@ -13,16 +16,14 @@ const Accordion = ({ header,data }) => {
 
   const styles = {
     wrapper: "drop-shadow-md bg-white",
-    header: "bg-[#828bb3] text-white text-lg font-semibold px-4 h-16 flex items-center",
-    title: "flex items-center h-12 border-b-2 font-semibold text-black px-4 hover:text-orange-500",
-    content: "pl-8 flex items-center h-12 border-b-2 text-black hover:text-orange-500",
+    header:"bg-[#828bb3] text-white text-lg font-semibold px-4 h-16 flex items-center",
+    title:"flex items-center h-12 border-b-2 font-semibold text-black px-4 hover:text-orange-500",
+    content:"pl-8 flex items-center h-12 border-b-2 text-black hover:text-orange-500",
   };
 
   return (
     <div>
-      <h4 className={styles.header}>
-        {header}
-      </h4>
+      <h4 className={styles.header}>{header}</h4>
       <ul className={styles.wrapper}>
         {data.map(({ title, content }, index) => (
           <li key={index}>
@@ -33,7 +34,11 @@ const Accordion = ({ header,data }) => {
             {selected === index ? (
               <ul>
                 {content.map(({ title }, index) => (
-                  <li key={index} className={styles.content}>
+                  <li
+                    key={index}
+                    className={styles.content}
+                    onClick={(e) => updateFilter(e)}
+                  >
                     {title}
                   </li>
                 ))}

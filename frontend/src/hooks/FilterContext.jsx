ebc90567ab5e1a1746e596e525f6a545dcbbef27
Manuel Nunez <manuelnunezalmonte@gmail.com>
2023-02-08 23:40:39 -0600

    Update FilterContext.jsx
    implement the use of object as values for the states and create  update function

--- frontend/src/hooks/FilterContext.jsx
+++ frontend/src/hooks/FilterContext.jsx
@@ -1,19 +1,47 @@
 import { useState, createContext, useContext } from "react";
 
 export const Filter = createContext();
+export const UpdateFilter = createContext();
+
 export const UseFilter = () => useContext(Filter);
+export const UseUpdateFilter = () => useContext(UpdateFilter);
 
 export const GenderProvider = ({ children }) => {
-
   const [filter, setFilter] = useState({
-    category: "",
-    gender: "women",
-    collection: "",
+    
+      category: "sports",
+      gender: "man",
+      collection: "",
+    
   });
+
+  const updateFilter = (e) => {
+ 
+ 
+    if (e.target.parentElement.parentElement.firstChild.innerText.toLowerCase() === "category") {
+      const value = e.target.innerText.toLowerCase()
+      setFilter(
+       { ...filter,
+        category: value}
+      )
+    }
+
+    if (e.target.parentElement.parentElement.firstChild.innerText.toLowerCase() === "gender") {
+      const value = e.target.innerText.toLowerCase()
+      setFilter(
+       { ...filter,
+        gender: value}
+      )
+    }
+
+
+  };
 
   return (
     <Filter.Provider value={filter}>
-      {children}
+      <UpdateFilter.Provider value={updateFilter}>
+        {children}
+      </UpdateFilter.Provider>
     </Filter.Provider>
   );
 };
